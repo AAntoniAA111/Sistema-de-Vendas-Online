456a15a2dc696d0c5ba82e7de2cc3c11075966ff
AntoniaQ <quintinodev01@gmail.com>
2026-08-26 16:14:53 -0300

    Delete src/main/java directory

--- src/main/java/App.java
+++
@@ -1,6 +0,0 @@
-import javax.swing.JOptionPane;
-public class App {
-    public static void main(String[] args) throws Exception {
-        JOptionPane.showMessageDialog(null, "Olá, Mundo!");
-    }
-}
